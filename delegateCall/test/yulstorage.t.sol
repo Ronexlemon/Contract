3791fc6511b58a433466762c75eec27e58070156
RonexLemon <ronexondimu@gmail.com>
2024-02-09 05:34:55 +0300

    add storage

---
+++ delegateCall/test/yulstorage.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.2;
+
+import {Test} from "forge-std/Test.sol";
+
+import {YulStorage2} from "../src/yul/yulStorage.sol";
+
+contract YulStorage2Test is Test{
+    YulStorage2 public contractYulstorage2;
+
+    function setUp()public{
+        contractYulstorage2 = new YulStorage2();
+    }
+
+   function test_getValAtSlot3ForVal5()public{
+
+    
+    assertEq(contractYulstorage2.readSlot3Val5(),5);
+      
+    }
+}
+
