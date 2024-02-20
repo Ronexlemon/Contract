64a6bd9faac1c1b2bba62e8cb13ae8f5c7a85bb0
RonexLemon <ronexondimu@gmail.com>
2024-02-20 04:45:50 +0300

    mload

--- delegateCall/src/yul/yulMemory.sol
+++ delegateCall/src/yul/yulMemory.sol
@@ -7,6 +7,7 @@ pragma solidity ^0.8.2;
 */
 
 contract YulMemory{
+    
 
     function mstoreExample()public pure{
         assembly{
@@ -20,6 +21,38 @@ contract YulMemory{
         assembly{
             mstore8(0x0,0x7)
             
+        }
+    }
+
+    function abiEncode()public pure{
+        bytes32 ptrStart;
+        bytes32 ptrEnd;
+        assembly{
+            ptrStart := mload(0x40)
+        }
+    bytes memory data =    abi.encode(uint256(12),uint256(13)); // 0xc, 0xd
+
+        assembly{
+            ptrEnd := mload(0x40)
+        }
+        // logHelper("ptr (start)",ptrStart,"");
+        //   logHelper("ptr (end)",ptrEnd,"");
+
+
+    }
+
+    function abiEncodePacked() public pure{
+        bytes32 ptrStart;
+        bytes32 ptrEnd;
+
+        assembly{
+            ptrStart := mload(0x40)
+        }
+
+        abi.encodePacked(uint256(14),uint128(15)); //0xe, 0xf
+
+        assembly{
+            ptrEnd := mload(0x40)
         }
     }
 
