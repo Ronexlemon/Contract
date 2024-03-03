5839e107ebbd752b73bed7b79ad7ac58ffd10031
RonexLemon <ronexondimu@gmail.com>
2024-03-03 14:05:26 +0300

    vm:expectRevert

--- delegateCall/src/simpleContract.sol
+++ delegateCall/src/simpleContract.sol
@@ -1,10 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
-
+error Unauthorized();
 contract SimpleContract{
     uint public  num;
+    address public owner;
+    constructor(){
+        owner = msg.sender;
+    }
 
     function setNum(uint _num)public{
+        if(msg.sender != owner){
+            revert Unauthorized();
+        }
         num= _num;
     }
 
--- delegateCall/test/simpleContract.t.sol
+++ delegateCall/test/simpleContract.t.sol
@@ -4,14 +4,32 @@ import {Test} from "forge-std/Test.sol";
 
 import {SimpleContract} from "../src/simpleContract.sol";
 
+ error Unauthorized();
 
 contract SimpleContractTest is Test{
     SimpleContract public simple;
+   
     function setUp()public{
         simple = new SimpleContract();
 
     }
     function test_initialnumIsZero()public{
         assertEq(simple.num(),0);
+        simple.setNum(4);
+        assertEq(simple.num(),4);
+
+
+    }
+
+    function testFail_setNotOwner()public{
+        vm.prank(address(0));
+        simple.setNum(5);
+
+    }
+
+    function test_Revert_when_CallnotTheOwner()public{
+        vm.expectRevert(Unauthorized.selector);
+        vm.prank(address(0));
+        simple.setNum(4);
     }
 }
