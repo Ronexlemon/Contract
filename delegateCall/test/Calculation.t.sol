f9637af1eed04cc97bdacfce094b96f08f08dabb
RonexLemon <ronexondimu@gmail.com>
2024-03-30 18:09:45 +0300

    init calculation test

---
+++ delegateCall/test/Calculation.t.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.2 ;
+
+import {Calculation} from "../src/calculation.sol";
+import {Test} from "forge-std/Test.sol";
+
+contract CalculationTest is Test{
+    
+}
+
+
