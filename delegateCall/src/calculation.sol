b779d8d421aa85236309acd61dfbfa63391f21df
RonexLemon <ronexondimu@gmail.com>
2024-03-31 11:44:42 +0300

    add:event

--- delegateCall/src/calculation.sol
+++ delegateCall/src/calculation.sol
@@ -4,7 +4,10 @@ pragma solidity ^0.8.2 ;
 error notOwner();
 
 contract Calculation{
+    event IncrementNum(address _caller, uint256 _value);
+     event DecrementNum(address _caller, uint256 _value);
     uint public num;
+  
     address public owner;
     constructor(){
         owner = msg.sender;
@@ -26,11 +29,13 @@ contract Calculation{
     //increment
     function increment()public{
         num ++;
+        emit IncrementNum(msg.sender, num);
     }
 
     //decrement
     function decrement()public{
         num --;
+        emit DecrementNum(msg.sender, num);
     }
 
     //getNum
